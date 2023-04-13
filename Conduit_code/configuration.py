5a08cdecbe8b255133487cf2180aadb76435b397
jgxpro <jgxpro@gmail.com>
2023-04-13 16:05:05 +0200

    Restoring correct configuration file.

--- Conduit_code/configuration.py
+++ Conduit_code/configuration.py
@@ -1,5 +1,5 @@
 # The target of the modul is to avoid the creation the appropriate Chrome driver in every occasion.
-# After import it's enough to call 'get_preconfigured_chrome_driver()' function.
+# After importing, it's enough to call 'get_preconfigured_chrome_driver()' function.
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -10,5 +10,8 @@ def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     s = Service(executable_path=ChromeDriverManager().install())
     o = Options()
     o.add_experimental_option('detach', True)
+    o.add_argument('--headless')
+    o.add_argument('--no-sandbox')
+    o.add_argument('--disable-dev-shm-usage')
 
     return webdriver.Chrome(service=s, options=o)
